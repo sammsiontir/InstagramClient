2e6e46c552311a9149c53d29616145d052c32fdc
sammsiontir <chengfu@umich.edu>
2016-02-05 22:38:23 -0800

    Add view holder to adapter

--- app/src/main/java/com/codepath/instagramclient/InstagramPhotosAdaper.java
+++ app/src/main/java/com/codepath/instagramclient/InstagramPhotosAdaper.java
@@ -3,7 +3,6 @@ package com.codepath.instagramclient;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.text.format.DateUtils;
-import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +19,33 @@ import java.util.List;
 public class InstagramPhotosAdaper extends ArrayAdapter<InstagramPhoto> {
     private InstagramPhoto photo;
 
+    public class CropSquareTransformation implements Transformation {
+        @Override public Bitmap transform(Bitmap source) {
+            int size = Math.min(source.getWidth(), source.getHeight());
+            int x = (source.getWidth() - size) / 2;
+            int y = (source.getHeight() - size) / 2;
+            Bitmap result = Bitmap.createBitmap(source, x, y, size, size);
+            if (result != source) {
+                source.recycle();
+            }
+            return result;
+        }
+
+        @Override public String key() { return "square()"; }
+    }
+
+    private static class ViewHolder {
+        ImageView ivProfilePicture;
+        TextView tvUsername;
+        TextView tvLocation;
+        TextView tvTime;
+        ImageView ivPhoto;
+        TextView tvLikes;
+        TextView tvCaption;
+    }
+
+
+
     public InstagramPhotosAdaper(Context context, List<InstagramPhoto> objects) {
         super(context, 0, objects);
     }
@@ -30,29 +56,36 @@ public class InstagramPhotosAdaper extends ArrayAdapter<InstagramPhoto> {
         // Get data item for this position
         photo = getItem(position);
         // Check if we are using recycle view. If not, we need to inflate
+        ViewHolder viewHolder;
         if(convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_photo, parent, false);
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_photo, parent, false);
+
+            // Initialize all the components in layout
+            viewHolder = new ViewHolder();
+            viewHolder.ivProfilePicture = (ImageView) convertView.findViewById(R.id.ivProfilePicture);
+            viewHolder.tvUsername = (TextView) convertView.findViewById(R.id.tvUsername);
+            viewHolder.tvLocation = (TextView) convertView.findViewById(R.id.tvLocation);
+            viewHolder.tvTime = (TextView) convertView.findViewById(R.id.tvTime);
+            viewHolder.ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
+            viewHolder.tvLikes = (TextView) convertView.findViewById(R.id.tvLikes);
+            viewHolder.tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
+            convertView.setTag(viewHolder);
         }
-        // Lookup the views for populating the data (image, Caption)
-        // Initialize all the components in layout
-        ImageView ivProfilePicture = (ImageView) convertView.findViewById(R.id.ivProfilePicture);
-        TextView tvUsername = (TextView) convertView.findViewById(R.id.tvUsername);
-        TextView tvLocation = (TextView) convertView.findViewById(R.id.tvLocation);
-        TextView tvTime = (TextView) convertView.findViewById(R.id.tvTime);
-        ImageView ivPhoto = (ImageView) convertView.findViewById(R.id.ivPhoto);
-        TextView tvLikes = (TextView) convertView.findViewById(R.id.tvLikes);
-        TextView tvCaption = (TextView) convertView.findViewById(R.id.tvCaption);
+        else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
 
         // Insert model data to each of the items
-        tvUsername.setText(photo.username);
-        tvLocation.setText("");
+        viewHolder.tvUsername.setText(photo.username);
+        viewHolder.tvLocation.setText("");
         CharSequence relativeTime = DateUtils.getRelativeTimeSpanString(photo.createTime*1000, System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS);
-        tvTime.setText(relativeTime);
-        tvLikes.setText(Integer.toString(photo.likesCount)  + "  Likes");
-        tvCaption.setText(photo.caption);
+        viewHolder.tvTime.setText(relativeTime);
+        viewHolder.tvLikes.setText(Integer.toString(photo.likesCount)  + "  Likes");
+        viewHolder.tvCaption.setText(photo.caption);
         // Insert image view using picasso
-        setProfilePicture(ivProfilePicture);
-        setPhoto(ivPhoto);
+        setProfilePicture(viewHolder.ivProfilePicture);
+        setPhoto(viewHolder.ivPhoto);
         // Return the create item as a view
         return convertView;
     }
@@ -82,26 +115,6 @@ public class InstagramPhotosAdaper extends ArrayAdapter<InstagramPhoto> {
                 .transform(new CropSquareTransformation())
                 .into(ivPhoto);
 
-    }
-
-    public static int getDisplayWidth(Context context) {
-        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
-        return displayMetrics.widthPixels;
-    }
-
-    public class CropSquareTransformation implements Transformation {
-        @Override public Bitmap transform(Bitmap source) {
-            int size = Math.min(source.getWidth(), source.getHeight());
-            int x = (source.getWidth() - size) / 2;
-            int y = (source.getHeight() - size) / 2;
-            Bitmap result = Bitmap.createBitmap(source, x, y, size, size);
-            if (result != source) {
-                source.recycle();
-            }
-            return result;
-        }
-
-        @Override public String key() { return "square()"; }
     }
 
 }
